c17b3e2896e780092f06cf00ac21cdf9d7d64f15
Daniel Abercrombie <dabercro@mit.edu>
2020-05-26 12:51:24 -0400

    Still not quite working

--- python/runMIT.sh
+++ python/runMIT.sh
@@ -10,7 +10,7 @@ scramv1 project CMSSW $CMSSW_VER
 
 cd $CMSSW_VER
 
-tar -xf ../xbb_condor.tgz
+# tar -xf ../xbb_condor.tgz
 
 cd src
 eval `scram runtime -sh`
@@ -20,10 +20,10 @@ cp ../../*.ini Xbb/python/.
 cd ../..
 
 cd $CMSSW_VER/src/Xbb/python
+rm -rf scratch
+rm -rf prep
+rm -rf sys
 
 ./runAll.sh run_ggZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8_part0 Zvv2018 run 1 noid --inputDir=PREPin --sampleIdentifier=ggZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8 --addCollections=Prep.VHbb --fileList=base64:eNoVzL0OgjAQAOAn0usdYnCEOMAgGEMYWC6QNC2J9pr+aHh7w/ZNH8QkQYMJkj14u0e2mzER7LqCS9m/dYSpXdenxNQvToAUVjAhgTFzy+0oTcPzKH3uMz+QSsZi1BN7+Vlt2O/JbksFr+y6rs4pfxxWx1MP9+/1dIjKAS8EpBQRMqEq8AZKKQUpaM14DiLpDxdGNys= --outputDir=PREPout --noretry
-
-cd $CMSSW_VER/src/Xbb/python
-rm scratch/*
 
 ./runAll.sh sysnew_ggZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8_part0 Zvv2018 sysnew 1 noid  --inputDir=SYSin  --sampleIdentifier=ggZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8  --addCollections=Sys.all  --fileList=base64:eNoVzL0OgjAQAOAn0usdYnCEOMAgGEMYWC6QNC2J9pr+aHh7w/ZNH8QkQYMJkj14u0e2mzER7LqCS9m/dYSpXdenxNQvToAUVjAhgTFzy+0oTcPzKH3uMz+QSsZi1BN7+Vlt2O/JbksFr+y6rs4pfxxWx1MP9+/1dIjKAS8EpBQRMqEq8AZKKQUpaM14DiLpDxdGNys=  --outputDir=SYSout --noretry
