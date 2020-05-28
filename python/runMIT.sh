8ddbe6604f5a0c6761519d23988aa6e695e4a165
Daniel Abercrombie <dabercro@mit.edu>
2020-05-28 10:04:56 -0400

    Remove duplicate HeppyStyleGen

--- python/runMIT.sh
+++ python/runMIT.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+filelist=
 
 export SCRAM_ARCH=slc6_amd64_gcc630
 export CMSSW_VER=CMSSW_10_1_0
@@ -10,7 +11,7 @@ scramv1 project CMSSW $CMSSW_VER
 
 cd $CMSSW_VER
 
-# tar -xf ../xbb_condor.tgz
+tar -xf ../xbb_condor.tgz
 
 cd src
 eval `scram runtime -sh`
@@ -20,10 +21,7 @@ cp ../../*.ini Xbb/python/.
 cd ../..
 
 cd $CMSSW_VER/src/Xbb/python
-rm -rf scratch
-rm -rf prep
-rm -rf sys
 
-./runAll.sh run_ggZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8_part0 Zvv2018 run 1 noid --inputDir=PREPin --sampleIdentifier=ggZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8 --addCollections=Prep.VHbb --fileList=base64:eNoVzL0OgjAQAOAn0usdYnCEOMAgGEMYWC6QNC2J9pr+aHh7w/ZNH8QkQYMJkj14u0e2mzER7LqCS9m/dYSpXdenxNQvToAUVjAhgTFzy+0oTcPzKH3uMz+QSsZi1BN7+Vlt2O/JbksFr+y6rs4pfxxWx1MP9+/1dIjKAS8EpBQRMqEq8AZKKQUpaM14DiLpDxdGNys= --outputDir=PREPout --noretry
+./runAll.sh run_ggZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8_part0 Zvv2018 run 1 noid --inputDir=PREPin --sampleIdentifier=ggZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8 --addCollections=Prep.VHbb --fileList=base64:eNoVzL0OgjAQAOAn0usdYnCEOMAgGEMYWC6QNC2J9pr+aHh7w/ZNH8QkQYMJkj14u0e2mzER7LqCS9m/dYSpXdenxNQvToAUVjAhgTFzy+0oTcPzKH3uMz+QSsZi1BN7+Vlt2O/JbksFr+y6rs4pfxxWx1MP9+/1dIjKAS8EpBQRMqEq8AZKKQUpaM10DiLpDxdMNyw= --outputDir=PREPout --noretry
 
-./runAll.sh sysnew_ggZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8_part0 Zvv2018 sysnew 1 noid  --inputDir=SYSin  --sampleIdentifier=ggZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8  --addCollections=Sys.all  --fileList=base64:eNoVzL0OgjAQAOAn0usdYnCEOMAgGEMYWC6QNC2J9pr+aHh7w/ZNH8QkQYMJkj14u0e2mzER7LqCS9m/dYSpXdenxNQvToAUVjAhgTFzy+0oTcPzKH3uMz+QSsZi1BN7+Vlt2O/JbksFr+y6rs4pfxxWx1MP9+/1dIjKAS8EpBQRMqEq8AZKKQUpaM14DiLpDxdGNys=  --outputDir=SYSout --noretry
+./runAll.sh sysnew_ggZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8_part0 Zvv2018 sysnew 1 noid  --inputDir=SYSin  --sampleIdentifier=ggZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8  --addCollections='VHbbCommon.EWKweights,VHbbCommon.BTagSFDeepCSV_18,VHbbCommon.HiggsReco,VHbbCommon.isGGZH,VHbbCommon.vLeptons,Sys.newBranches,Sys.fill1,Sys.fill2,Sys.fill3,VHbbCommon.LOtoNLOweight,VHbbCommon.TTweights,VHbbCommon.DYspecialWeight,VHbbCommon.JetPUIDSF,Sys.metTriggerSF,Sys.PUweight,VHbbCommon.DoubleBtagSF,VHbbCommon.HeppyStyleGen,VHbbCommon.METcorr' --fileList=base64:eNoVzL0OgjAQAOAn0usdYnCEOMAgGEMYWC6QNC2J9pr+aHh7w/ZNH8QkQYMJkj14u0e2mzER7LqCS9m/dYSpXdenxNQvToAUVjAhgTFzy+0oTcPzKH3uMz+QSsZi1BN7+Vlt2O/JbksFr+y6rs4pfxxWx1MP9+/1dIjKAS8EpBQRMqEq8AZKKQUpaM10DiLpDxdMNyw= --outputDir=SYSout --noretry
